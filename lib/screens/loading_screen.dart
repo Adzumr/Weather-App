cf51342c91cf6dc58f9fffae38f965179300a4c4
Abdulaziz Umar Jada (Adzumr) <adzumrjada@gmail.com>
2022-03-06 23:16:31 +0100

    update on network data

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -3,6 +3,7 @@ import 'dart:developer';
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:weather_app/services/location.dart';
+import 'package:http/http.dart' as http;
 
 class LoadingScreen extends StatefulWidget {
   const LoadingScreen({Key? key}) : super(key: key);
@@ -12,26 +13,41 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+  late double latitude;
+  late double longitude;
   @override
   void initState() {
     super.initState();
     getLocation();
   }
 
+  Location location = Location();
   void getLocation() async {
-    Location location = Location();
     await location.getCurrentLocation();
-    log(location.latitude.toString());
-    log(location.longitude.toString());
+    latitude = location.latitude;
+    longitude = location.longitude;
+  }
+
+  void getData() async {
+    try {
+      var url = Uri.parse(
+        'https://api.openweathermap.org/data/2.5/weather?lat=${location.latitude}&lon=${location.longitude}&appid=ccc00165de8167fbef1e32dc45387785',
+      );
+      http.Response response = await http.get(url);
+      print(response.statusCode);
+    } catch (e) {
+      print(e);
+    }
   }
 
   @override
   Widget build(BuildContext context) {
+    getData();
     return Scaffold(
       body: Center(
         child: TextButton(
           onPressed: () {},
-          child: const Text("Get User Current Location"),
+          child: const Text("Get Location"),
         ),
       ),
     );

2cb4adaa5915ca57f6afd43ef2923488aec897e0
Abdulaziz Umar Jada (Adzumr) <adzumrjada@gmail.com>
2024-08-08 22:30:36 +0100

    Upgrade SDK

--- lib/controllers/permission_handler.dart
+++
@@ -1,8 +0,0 @@
-import 'package:permission_handler/permission_handler.dart';
-
-class Permissions {
-  static Future<bool> requestLocationPermission() async {
-    PermissionStatus status = await Permission.location.request();
-    return status == PermissionStatus.granted;
-  }
-}
